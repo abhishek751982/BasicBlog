84ee1359f5f6ad4358b91d40afa86417560126ee
Abhishek Sharma <abhishek751982@gmail.com>
2024-02-11 15:23:59 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
