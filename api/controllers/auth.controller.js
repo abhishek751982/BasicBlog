4d80576374331a72a4bd0acc427e5e040a30a959
Abhishek Sharma <abhishek751982@gmail.com>
2024-03-13 02:31:45 +0530

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,7 +44,7 @@ export const signin = async (req, res, next) => {
             return next(errorHandler(400, 'Invalid password'));
         }
         
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: validUser._id, isAdmid: validUser.isAdmin }, process.env.JWT_SECRET);
 
         const { password: pass, ...rest } = validUser._doc
         res.status(200).cookie('access_token', token, { 
@@ -60,7 +60,7 @@ export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({ email });
         if(user) {
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
             const { password, ...rest } = user._doc;
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
@@ -76,7 +76,7 @@ export const google = async (req, res, next) => {
                 profilePicture: googlePhotoUrl,
             });
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
             const { password, ...rest } = newUser._doc;
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,25 +1,31 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    profilePicture: {
-        type: String,
-        dafault: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw3eItajILiv1uYLU8CPNZpp&ust=1708379785888000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJimnePwtYQDFQAAAAAdAAAAABAE",
-    },
-}, {timestamps: true}
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profilePicture: {
+            type: String,
+            dafault: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw3eItajILiv1uYLU8CPNZpp&ust=1708379785888000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJimnePwtYQDFQAAAAAdAAAAABAE",
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+    }, 
+    { timestamps: true }
 );
 
 const User = mongoose.model('User', userSchema);
