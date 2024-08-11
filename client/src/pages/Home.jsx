0a364a97432cea623afb4188129433d02282af7a
Abhishek Sharma <abhishek751982@gmail.com>
2024-08-11 22:40:55 +0530

    complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,13 +7,9 @@ export default function Home() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     const fetchPosts = async () => {
-      try {
-        const res = await fetch('/api/post/getposts');
-        const data = await res.json();
-        setPosts(data.posts);
-      } catch (error) {
-        console.log(error.message);
-      }
+      const res = await fetch('/api/post/getposts');
+      const data = await res.json();
+      setPosts(data.posts); 
     }
     fetchPosts();
   }, []);
@@ -21,7 +17,7 @@ export default function Home() {
     <div>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
-        <p className="text-gray-500 text-xs sm:tezt-sm">Here you will find a variety of articles and tutorials on topics such as web development, software engineering, and programming languages.</p>
+        <p className="text-gray-500 text-xs sm:text-sm">Here you will find a variety of articles and tutorials on topics such as web development, software engineering, and programming languages.</p>
         <Link to="/search" className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>
           View all posts
         </Link>
@@ -34,16 +30,16 @@ export default function Home() {
           posts && posts.length > 0 && (
             <div className='flex flex-col gap-6'>
               <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
-              <div className='flex flex-wrap gap-4'>
+              <div className='flex flex-wrap gap-4 justify-center max-w-7xl'>
                 {
                   posts.map((post) => (
                     <PostCard key={post._id} post={post} />
                   ))
                 }
               </div>
-                <Link to="/search" className='text-lg text-teal-500 hover:underline text-center'>
-                  View all posts
-                </Link>
+              <Link to="/search" className='text-lg text-teal-500 hover:underline text-center'>
+                View all posts
+              </Link>
             </div>
           )
         }
