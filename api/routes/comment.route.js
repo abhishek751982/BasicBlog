6c14fc0e06d325957df54d3311e432a4846b24b3
Abhishek Sharma <abhishek751982@gmail.com>
2024-08-11 13:12:47 +0530

    add comments to the admin dashboard

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, getPostComments, likeComment, editComment, deleteComment } from '../controllers/comment.controller.js';
+import { 
+  createComment, 
+  getPostComments, 
+  likeComment, 
+  editComment, 
+  deleteComment, 
+  getcomments 
+} from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
@@ -9,5 +16,6 @@ router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
 router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken, getcomments);
 
 export default router;
