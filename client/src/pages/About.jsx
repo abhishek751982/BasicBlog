4d899942472104c7529d7b39113946f900a08be0
Abhishek Sharma <abhishek751982@gmail.com>
2024-08-12 01:09:38 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,24 @@
 export default function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">About BasicBlog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to BasicBlog! This blog was created to share his thoughts and ideas with the world. I am a passionate software developer and I love to write about my experiences and things that I have learned.
+              I hope you enjoy reading my blog.
+            </p>
+            <p>
+              On this blog, you will find weekly articles and tutorials on topics such as web development, software engineering, and programming languages. I always learning and exploring new technologies, so be sure to check back often for new content!
+            </p>
+            <p>
+              We encourage you to leave comments on our posts and engage with other readers. You can like other people's comments and reply to them as well. We believe that a community of learners can help each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
