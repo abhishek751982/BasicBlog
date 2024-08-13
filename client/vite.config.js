be4f5cfcc3170d9a3193259cfc1851f0e1b8df70
Abhishek Sharma <abhishek751982@gmail.com>
2024-08-13 14:19:35 +0530

    update backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,22 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // server: {
+  //   proxy: {
+  //     '/': {
+  //       target: 'https://basicblog-1.onrender.com',
+  //       secure: false,
+  //     },
+  //   },
+  // },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
-      },
+    '/api': {
+      target: 'https://basicblog-1.onrender.com',
+      changeOrigin: true,
+      secure: true,
+    },
+    // cors:false
     },
   },
   plugins: [react()],
