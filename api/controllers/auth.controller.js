cb9b6a3d43a444633d70c616bc8678e6d43dd3f2
Abhishek Sharma <abhishek751982@gmail.com>
2024-10-06 02:50:27 +0530

    change api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,7 +44,7 @@ export const signin = async (req, res, next) => {
             return next(errorHandler(400, 'Invalid password'));
         }
         
-        const token = jwt.sign({ id: validUser._id, isAdmid: validUser.isAdmin }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET);
 
         const { password: pass, ...rest } = validUser._doc
         res.status(200).cookie('access_token', token, { 
